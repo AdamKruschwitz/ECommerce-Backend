939b9a8bd5aa5458b9f0588984ec892b8cebba16
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-27 15:48:07 -0700

    added tag/id delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,7 +6,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-  // TODO: Fix associated product data
   try {
     const tags = await Tag.findAll({
       include: [{model: Product}]
@@ -47,6 +46,17 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!tag) res.status(404).json("Tag not found");
+    else res.status(200).json("Tag destroyed sucessfully.");
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
