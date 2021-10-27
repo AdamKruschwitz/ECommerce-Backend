12f9e043d0817fb278ef1e15ef38b7c529c0fdfb
Adam Kruschwitz <a.j.kruschwitz@gmail.com>
2021-10-27 15:26:57 -0700

    added category/id delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { response } = require('express');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -18,7 +19,7 @@ router.get('/', async (req, res) => {
   res
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
@@ -33,12 +34,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryFound = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!categoryFound) res.status(404).json("Category not found");
+    else res.status(200).json("category deleted successfully");
+  } catch (err) {
+    response.status(500).json(err);
+  }
 });
 
 module.exports = router;
